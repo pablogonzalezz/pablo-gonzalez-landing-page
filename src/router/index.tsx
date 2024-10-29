9f6e8da25cfd50ee2f2352fdba23526526781887
Pablo Morales <pablo.morales@newhotel.com>
2024-10-29 12:41:44 +0000

    add hashrouter

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,5 +1,5 @@
 import { lazy, Suspense } from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, HashRouter } from "react-router-dom";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import routes from "./config";
@@ -18,18 +18,13 @@ const Router = () => {
       }
     >
       <Header />
-      <Switch>
-        {routes.map((routeItem) => {
-          return (
-            <Route
-              key={routeItem.component}
-              path={routeItem.path}
-              exact={routeItem.exact}
-              component={lazy(() => import(`../pages/${routeItem.component}`))}
-            />
-          );
-        })}
-      </Switch>
+      <HashRouter>
+        <Route
+          path={["/", "/home"]}
+          exact={true}
+          component={lazy(() => import(`../pages/Home`))}
+        />
+      </HashRouter>
       <Footer />
     </Suspense>
   );
