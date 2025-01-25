92106c4f5c7f02cc62f546ea07a290e04488b622
Pablo <pablo@MacBook-Pro-de-Pablo.local>
2025-01-25 17:38:59 +0000

    Updates

---
+++ static/js/1766.97562b86.chunk.js
@@ -0,0 +1,24 @@
+"use strict";(self.webpackChunkpablo_gonzalez_landing_page=self.webpackChunkpablo_gonzalez_landing_page||[]).push([[1766],{81766:(a,d,e)=>{e.r(d),e.d(d,{default:()=>p});const n=(0,e(30009).Ay)("div")`
+  position: relative;
+  width: 100%;
+  // max-width: 1200px;
+  margin-right: auto;
+  margin-left: auto;
+  border-top: ${a=>a.border?"1px solid #CDD1D4":""};
+
+  // @media only screen and (max-width: 1024px) {
+  //   max-width: calc(100% - 68px);
+  //   padding: 0 30px;
+  // }
+
+  // @media only screen and (max-width: 768px) {
+  //   max-width: calc(100% - 38px);
+  //   padding: 0 18px;
+  // }
+
+  // @media only screen and (max-width: 414px) {
+  //   max-width: 100%;
+  //   padding: 0 18px;
+  // }
+`;var i=e(70579);const p=a=>{let{border:d,children:e}=a;return(0,i.jsx)(n,{border:d,children:e})}}}]);
+//# sourceMappingURL=1766.97562b86.chunk.js.map
